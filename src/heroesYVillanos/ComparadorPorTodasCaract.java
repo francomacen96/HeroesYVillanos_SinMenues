acd59b674346df2551a7dce7f017ce20a8e32aa1
Franco Macen <macen.franco.38787@gmail.com>
2022-06-27 13:35:23 -0300

    Cambios en el comparador

--- src/heroesYVillanos/ComparadorPorTodasCaract.java
+++ src/heroesYVillanos/ComparadorPorTodasCaract.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 public class ComparadorPorTodasCaract implements Comparator<Combatiente>{
 	@Override
 	public int compare(Combatiente c1, Combatiente c2){
+		int retorno = 0;
 		boolean mismaVelocidad = c1.getCaracteristica(Caracteristica.VELOCIDAD) == c2.getCaracteristica(Caracteristica.VELOCIDAD);
 		boolean mismaFuerza = c1.getCaracteristica(Caracteristica.FUERZA) ==  c2.getCaracteristica(Caracteristica.FUERZA);
 		boolean mismaResistencia = c1.getCaracteristica(Caracteristica.RESISTENCIA) == c2.getCaracteristica(Caracteristica.RESISTENCIA);
@@ -16,21 +17,22 @@ public class ComparadorPorTodasCaract implements Comparator<Combatiente>{
 		System.out.println("Misma resistencia: " + mismaResistencia);
 		
 		try {
-			if(mismaVelocidad && mismaFuerza && mismaDestreza) {
+			if(mismaVelocidad && mismaFuerza && mismaResistencia) {
 				System.out.println("If 1");
-				return -Integer.compare(c1.getCaracteristica(Caracteristica.DESTREZA), c2.getCaracteristica(Caracteristica.DESTREZA));
-			} else if(mismaVelocidad && mismaFuerza && !mismaDestreza) {
+				retorno =  -Integer.compare(c1.getCaracteristica(Caracteristica.DESTREZA), c2.getCaracteristica(Caracteristica.DESTREZA));
+			} else if(mismaVelocidad && mismaFuerza && !mismaResistencia) {
 				System.out.println("If 2");
-				return -Integer.compare(c1.getCaracteristica(Caracteristica.RESISTENCIA), c2.getCaracteristica(Caracteristica.RESISTENCIA));
+				retorno =  -Integer.compare(c1.getCaracteristica(Caracteristica.RESISTENCIA), c2.getCaracteristica(Caracteristica.RESISTENCIA));
 			} else if( mismaVelocidad && !mismaFuerza) {
 				System.out.println("If 3");
-				return -Integer.compare(c1.getCaracteristica(Caracteristica.FUERZA), c2.getCaracteristica(Caracteristica.FUERZA));
+				retorno =  -Integer.compare(c1.getCaracteristica(Caracteristica.FUERZA), c2.getCaracteristica(Caracteristica.FUERZA));
 			} else if(!mismaVelocidad){
 				System.out.println("If 4");
-				return -Integer.compare(c1.getCaracteristica(Caracteristica.VELOCIDAD), c2.getCaracteristica(Caracteristica.VELOCIDAD));
+				retorno =  -Integer.compare(c1.getCaracteristica(Caracteristica.VELOCIDAD), c2.getCaracteristica(Caracteristica.VELOCIDAD));
 			} else {
 				throw new ErrorDeComparacionException();
 			}
+			return retorno;
 		}catch(ErrorDeComparacionException E){
 			System.err.println("Hubo un error en la comparacion");
 			return 0;
