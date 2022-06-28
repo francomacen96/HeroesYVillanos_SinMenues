0049fbdaacc08eb0f3ea859186db20cfb5812104
Samuel Castro <88595777+NadalconHaste@users.noreply.github.com>
2022-06-27 21:42:47 -0300

    Update Combatiente.java

--- src/heroesYVillanos/Combatiente.java
+++ src/heroesYVillanos/Combatiente.java
@@ -20,39 +20,33 @@ public abstract class Combatiente implements Comparable<Combatiente>{
 	}
 
 	public boolean esGanador(Combatiente combatiente, Caracteristica c) {
+		boolean esGanador = false;
+		int contador = 0;
+
 		try {
 			examinarEquipo(combatiente, c);
+			
+			while (this.getCaracteristica(c) == combatiente.getCaracteristica(c) && contador < 4) {
+				c = c.nextCaracteristica(c);
+				contador++;
+			}
+			if (this.getCaracteristica(c) > combatiente.getCaracteristica(c)) {
+				esGanador = true;
+				System.out.println("El combatiente " + this.getNombre() + " es el vencedor del combate por " + c);
+			} else if (this.getCaracteristica(c) < combatiente.getCaracteristica(c)) {
+				System.out.println(
+						"El combatiente " + combatiente.getNombre() + " es el vencedor del combate por " + c);
+			} else {
+				System.out.println("El resultado de la pelea es empate en todas las caracteristicas");
+			}
+
 		} catch (PeleasEntreElMismoEquipoExcepcion e) {
 			System.err.println("No se pueden enfrentar 2 combatientes del mismo equipo!!!");
 		}
 
-		boolean esGanador = false;
-		Caracteristica aux = c;
-
-		if (!this.getEquipo().equals(combatiente.getEquipo())) {
-
-			while (this.getCaracteristica(aux) == combatiente.getCaracteristica(aux)) {
-				aux = aux.nextCaracteristica(aux);
-
-				if (aux.equals(c)) {
-					System.out.println("El resultado de la pelea es empate en todas las caracteristicas");
-					return esGanador;
-				}
-
-			}
-			if (this.getCaracteristica(aux) > combatiente.getCaracteristica(aux)) {
-				esGanador = true;
-				System.out.println("El combatiente " + this.getNombre() + " es el vencedor del combate por " + aux);
-
-			} else {
-				System.out.println(
-						"El combatiente " + combatiente.getNombre() + " es el vencedor del combate por " + aux);
-			}
-
-		}
-
 		return esGanador;
 	}
+
 	
 	private boolean examinarEquipo(Combatiente c, Caracteristica caracteristica) throws PeleasEntreElMismoEquipoExcepcion {
 		boolean diferenteEquipo = true;
@@ -77,16 +71,6 @@ public abstract class Combatiente implements Comparable<Combatiente>{
 	public Equipo getEquipo() {
 		return equipo;
 	}
-
-	/* @delete
-	public int compareTo(Liga otraLiga, Caracteristica c) {
-		return Integer.compare(this.getCaracteristica(c), otraLiga.getCaracteristica(c));
-	}
-	*/
-	
-	/*public int compareTo(Combatiente otroCombatiente, Caracteristica c) { //@todo borrar o cambiar
-		return Integer.compare(this.getCaracteristica(c), otroCombatiente.getCaracteristica(c));
-	}*/
 
 	/*
 	 * public int compareTo(Combatiente otroCombatiente) : 
